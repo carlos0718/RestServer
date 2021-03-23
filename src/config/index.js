8660a1c7618ecdd7a77dc82211deee844ff93caf
Carlos Jesus <c.sipiran07@gmail.com>
2021-03-23 19:14:37 -0300

    se actualiza codigo

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const envFound = dotenv.config();
 
 if(!envFound){
-    throw new Error("Couldn't find .env file.");
+    throw new Error('Couldn\'t find .env file.');
 }
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
